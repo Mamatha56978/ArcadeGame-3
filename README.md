aab7fd23afdcc1e4544ef15250f64aea19ad860a
Mamatha56978 <mamatharoyalbk@gmail.com>
2019-06-19 12:36:26 +0530

    Success

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# ArcadeGame-3
+# Classic Arcade Game Clone Project
+ I learned how to use prototype method. This is very interesting game. This is completely different from the games I had played ever.  I enjoyed a lot while playing this game.
+ I had learned JavaScript better while developing this game.
+
+## Table of Contents
+
+- [Instructions](#instructions)
+- [Contributing](#contributing)
+
+## Instructions
+
+Use this [rubric](https://review.udacity.com/#!/rubrics/15/view) for self-checking your submission.
+
+Make sure the functions you write are **object-oriented** - either class functions (like `Player` and `Enemy`) or class prototype functions such as `Enemy.prototype.checkCollisions`. Also make sure that the keyword `this` is used appropriately within your class and class prototype functions to refer to the object the function is called upon.
+
+Your **README.md** file should be updated with instructions on both how to 1. Run and 2. Play your arcade game.
+
+For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+
+## Contributing
+
+This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+
+## I have followed the below steps to develop the Arcade Game
+
+1. I had downloaded the skeleton of the project from the GitHub link which was given by the Udacity https://github.com/udacity/frontend-nanodegree-arcade-game.
+2. Then I extracted the downloaded zip file from which I came to understand that many modifications would happen in `app.js`.
+3. After that I have rectified few errors from which I got the canvas.
+4. Then I got the bugs and player by rectifying few more errors.
+5. After that I have created a class called `Fighter` with functions `update` and `render` and fixed the position of the player.
+6. Then I provided the definition for the `handleInput` function for the moment of the player.
+7. After that I have adjusted the bugs initial position by providing the values in the array called `allEnemies[]`.
+8. Then I provided the code for the moment of bugs with random speed using `Math.random()` function.
+9. If the player touches the bug, he will come to initial position.
+10. If the player touches the bug for 30 times, he will get an alert to start his game again and he will loose all his score.
+11. I have used `alert()` function to give an alert for the player.
+12. To give the score for the player, I have used `span` tag.
+13. If his score increases for two times then the player character changes.
+14. If the player reaches the water block without touching the bugs then he will gain a point.
---
+++ js/app.js
@@ -0,0 +1,131 @@
+"use strict"
+// Enemies our player must avoid
+var Enemy = function(x, y, speed) {
+	// Variables applied to each of our instances go here,
+	// we've provided one for you to get started
+	this.x = x;
+	this.y = y;
+	this.speed = speed;
+	// The image/sprite for our enemies, this uses
+	// a helper we've provided to easily load images
+	this.sprite = 'images/enemy-bug.png';
+};
+var touch = 0;
+var score1 = document.getElementById("score");
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+	// You should multiply any movement by the dt parameter
+	// which will ensure the game runs at the same speed for
+	// all computers.
+	this.x = this.x + this.speed * dt;
+	if (this.x > 500) {
+		this.x = 0;
+		this.speed = 100 + Math.floor(Math.random() * 400);
+	}
+	if (player.x < this.x + 80 && player.x + 80 > this.x && player.y < this.y + 80 && player.y + 80 > this.y) {
+		player.x = 200;
+		player.y = 400;
+		touch = touch + 1;
+		if (touch == 30) {
+			alert("Game over. Start again");
+			player.x = 200;
+			player.y = 400;
+			score = 0;
+      score1.innerHTML = score;
+			touch = 0;
+		}
+	}
+};
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
+class Fighter {
+	constructor(x, y, sprite) {
+		this.sprite = 'images/char-cat-girl.png';
+		this.y = y;
+		this.x = x;
+	}
+}
+Fighter.prototype.update = function(dt) {}
+Fighter.prototype.render = function() {
+	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
+var allEnemies = [];
+var item = new Enemy(-40, 60, 150);
+var item1 = new Enemy(120, 143, 150);
+var item2 = new Enemy(-90, 226, 150);
+allEnemies.push(item, item1, item2);
+var player = new Fighter(200, 400);
+var score = 0;
+// Code for the movement of the player
+Fighter.prototype.handleInput = function(key) {
+	if (key == 'left') {
+		if (this.x >= 0) this.x = this.x - 101;
+	}
+	if (key == 'right') {
+		if (this.x < 303) this.x = this.x + 101;
+	}
+	if (key == 'up') {
+		if (this.y >= 60) this.y = this.y - 83;
+		if (this.y < 60) {
+			setTimeout(() => {
+				this.x = 200;
+				this.y = 400;
+				score = score + 1;
+				score1.innerHTML = score;
+				if (score == 2) {
+					this.sprite = 'images/char-boy.png';
+					document.getElementById("img2").style.visibility = "visible";
+					document.getElementById("img1").style.opacity = "0.5";
+					document.getElementById("img2").style.opacity = "1.0";
+				}
+				if (score == 4) {
+					this.sprite = 'images/char-horn-girl.png';
+					document.getElementById("img3").style.visibility = "visible";
+					document.getElementById("img1").style.opacity = "0.5";
+					document.getElementById("img2").style.opacity = "0.5";
+					document.getElementById("img3").style.opacity = "1.0";
+				}
+				if (score == 6) {
+					this.sprite = 'images/char-pink-girl.png';
+					document.getElementById("img4").style.visibility = "visible";
+					document.getElementById("img1").style.opacity = "0.5";
+					document.getElementById("img2").style.opacity = "0.5";
+					document.getElementById("img3").style.opacity = "0.5";
+					document.getElementById("img4").style.opacity = "1.0";
+				}
+				if (score == 8) {
+					this.sprite = 'images/char-princess-girl.png';
+					document.getElementById("img5").style.visibility = "visible";
+					document.getElementById("img1").style.opacity = "0.5";
+					document.getElementById("img2").style.opacity = "0.5";
+					document.getElementById("img3").style.opacity = "0.5";
+					document.getElementById("img4").style.opacity = "0.5";
+					document.getElementById("img5").style.opacity = "1.0";
+				}
+			}, 80)
+		}
+	}
+	if (key == 'down') {
+		if (this.y < 380) this.y = this.y + 83;
+	}
+}
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+	var allowedKeys = {
+		37: 'left',
+		38: 'up',
+		39: 'right',
+		40: 'down'
+	};
+	player.handleInput(allowedKeys[e.keyCode]);
+});
